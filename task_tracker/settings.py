252da2ff2ef387f9b01ab0b3e7de755a892a7a74
Savelev Maxim <vismanmark@yandex.ru>
2025-09-04 20:26:42 +0300

    ci test

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = "task_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("POSTGRES_ENGINE"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
